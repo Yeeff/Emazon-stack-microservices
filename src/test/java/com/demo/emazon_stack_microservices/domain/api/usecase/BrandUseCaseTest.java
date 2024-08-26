fd2754d502dc060e6bf27aad7be9e272ccedd3da
Yeeff <yefrygf@gmail.com>
2024-08-26 08:50:39 -0500

    test: Add unit tests for Brand pagination functionality
    - Implemented unit tests to verify the pagination logic for fetching Brand entities by name.

--- src/test/java/com/demo/emazon_stack_microservices/domain/api/usecase/BrandUseCaseTest.java
+++ src/test/java/com/demo/emazon_stack_microservices/domain/api/usecase/BrandUseCaseTest.java
@@ -12,6 +12,7 @@ import org.mockito.MockitoAnnotations;
 import java.util.Collections;
 import java.util.List;
 
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -57,13 +58,36 @@ class BrandUseCaseTest {
                 new Brand(1L, "LG", "All electronic brands")
         );
 
-        when(brandPersistencePort.getAllBrands()).thenReturn(brands);
+        when(brandPersistencePort.getAllBrands(anyInt(),anyInt(),anyString())).thenReturn(brands);
 
-        List<Brand> result = brandUseCase.getAllBrands();
+        List<Brand> result = brandUseCase.getAllBrands(1,0,"ASC");
 
         assertEquals(1, result.size());
         assertEquals("LG", result.get(0).getName());
-        verify(brandPersistencePort, times(1)).getAllBrands();
+        verify(brandPersistencePort, times(1)).getAllBrands(1,0,"ASC");
+    }
+
+    @Test
+    void testGetAllCategoriesWithValidPaginationParameters() {
+
+        Integer size = 5;
+        Integer page = 0;
+        String sortDirection = "ASC";
+
+        List<Brand> expectedBrands = List.of(
+                new Brand(1L, "Category1", "Description1"),
+                new Brand(2L, "Category2", "Description2")
+        );
+
+        when(brandPersistencePort.getAllBrands(size, page, sortDirection)).thenReturn(expectedBrands);
+
+        List<Brand> actualBrands = brandUseCase.getAllBrands(size, page, sortDirection);
+
+        assertNotNull(actualBrands, "The returned brand list should not be null.");
+        assertEquals(expectedBrands.size(), actualBrands.size(), "The size of the returned brand list should match the expected size.");
+        assertEquals(expectedBrands, actualBrands, "The returned brand list should match the expected brands.");
+
+        verify(brandPersistencePort, times(1)).getAllBrands(size, page, sortDirection);
     }
 
 }
